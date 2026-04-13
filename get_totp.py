ac170347a93fe80c2ed89ca253b6a92f59f485cc
PangaPangaDKF <jbeykerj@gmail.com>
2026-04-13 06:14:32 -0400

    chore: clean repo, remove old files and screenshots

--- get_totp.py
+++
@@ -1,8 +0,0 @@
-import pyotp
-from vault import SecureVault
-
-vault = SecureVault()
-creds = vault.load_credentials()
-
-totp = pyotp.TOTP(creds['binance']['totp_secret'])
-print(f"Código TOTP Binance: {totp.now()}")
